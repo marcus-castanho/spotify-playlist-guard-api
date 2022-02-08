11e0f5549e617abff7b278bd31b2a963bd07f86c
Marcus <65380139+marcus-castanho@users.noreply.github.com>
2022-02-07 23:58:56 -0300

    Create entity User module structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [UsersModule, ConfigModule.forRoot()],
   controllers: [],
   providers: [],
 })
