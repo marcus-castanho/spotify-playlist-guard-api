8feca4da3ff1ecbb79647a41b6c7bf32e754f0e8
Marcus <65380139+marcus-castanho@users.noreply.github.com>
2022-02-12 15:26:22 -0300

    Delete repository implementation
    -Import PrismaModule directly into other modules to interact with the DB

--- src/users/repository/IUsersRepository.ts
+++
@@ -1,7 +0,0 @@
-import { User } from '../entity/user.entity';
-
-interface IUsersRepository {
-  create(user: User): Promise<void>;
-}
-
-export { IUsersRepository };
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
-import { UsersPostgresRepository } from './repository/implementation/UserPostgresRepository';
 
 @Module({
   controllers: [UsersController],
