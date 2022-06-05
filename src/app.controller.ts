3f75f00553cce1254519135071fe432b8900db9b
Marcus <65380139+marcus-castanho@users.noreply.github.com>
2022-06-05 15:48:35 -0300

    Add docs to root route
    -In app.controller.ts, add swagger decorators for documenting the root
    route;

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,14 +1,22 @@
-import { Controller, Get, Req, Res } from '@nestjs/common';
+import { Controller, Get, HttpCode, Req, Res } from '@nestjs/common';
+import { ApiFoundResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { Request, Response } from 'express';
 import { Public } from './auth/decorators/public.decorator';
 
 @Public()
+@ApiTags('Root')
 @Controller()
 export class AppController {
   constructor() {
     return;
   }
 
+  @ApiOperation({
+    summary:
+      "Accesses to this rout will be redirected to the API docs at '/api'.",
+  })
+  @ApiFoundResponse()
+  @HttpCode(302)
   @Get()
   redirectRootToDocs(@Req() req: Request, @Res() res: Response) {
     return res.redirect('/api');
